d5e85659799ee9d07d9ef2073d8d5a3a6bbd762c
GiangPhan <phanbagiang2504@gmail.com>
2018-06-09 16:26:18 +0700

    Log in google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-
     if user && user.authenticate(params[:session][:password])
       if user.admin?
         process_login user
@@ -29,6 +28,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
     log_out
     flash[:success] = t ".logout"
     redirect_to login_path
