bda1d1aeb014fa6c3d0141c38e932f1fc78ef65c
PhamKhanhDong <khanhdong95vip@gmail.com>
2018-06-10 10:35:44 +0700

    sidekiq email

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,12 @@
 class Admin::BaseController < ApplicationController
   layout "admin"
+  before_action :check_admin
+
+  private
+
+  def check_admin
+    return if logged_in? && current_user.admin?
+    flash[:warning] = t ".warning"
+    redirect_to root_path
+  end
 end
